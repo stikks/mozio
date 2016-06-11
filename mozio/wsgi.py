fea30f5080ace5314946dfd01515337ead74eeea
Ademola Oladipo <styccs@gmail.com>
2016-06-11 16:17:57 +0100

    commit

--- mozio/wsgi.py
+++ mozio/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mozio.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mozio.settings.prodcution")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
