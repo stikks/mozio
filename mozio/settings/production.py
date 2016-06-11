20c891c7b954d76eb0b32983a483b6fea9f71d5a
Ademola Oladipo <styccs@gmail.com>
2016-06-11 16:31:10 +0100

    commit

--- mozio/settings/production.py
+++ mozio/settings/production.py
@@ -1,12 +1,6 @@
 __author__ = 'stikks'
+from .local import *
 
 import dj_database_url
-from .base import *
-
-DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
-# DATABASES = dict()
-#
-# DATABASES['default'] = dj_database_url.config()
-# DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
-
-ALLOWED_HOSTS = ['*']
+DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
