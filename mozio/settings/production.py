17e2ca11e9378a49d0fd1eb9f1f6b0dfd746f10d
Ademola Oladipo <styccs@gmail.com>
2016-06-11 16:48:34 +0100

    commit

--- mozio/settings/production.py
+++ mozio/settings/production.py
@@ -2,5 +2,5 @@ __author__ = 'stikks'
 from .local import *
 
 import dj_database_url
-POSTGRES_URL = "HEROKU_POSTGRESQL_<DB_NAME>_URL"
+POSTGRES_URL = "postgres://hykhvgqncmpxqa:FVHYS7iCEiJ565wfM-QPVLPkNo@ec2-54-235-65-139.compute-1.amazonaws.com:5432/d1f475em3up1gj"
 DATABASES = {'default': dj_database_url.config(default=os.environ[POSTGRES_URL])}
